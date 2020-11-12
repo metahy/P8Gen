ae2a0ab818fd02afefcc155598432f44aae815bf
hiramjoyce <chliang0904@sina.com>
2020-11-12 17:39:25 +0800

    split camelcase to big and small

--- src/com/tools/gen/utils/CamelCaseUtils.java
+++ src/com/tools/gen/utils/CamelCaseUtils.java
@@ -2,7 +2,26 @@ package com.tools.gen.utils;
 
 public class CamelCaseUtils {
 
-    public static String toCamelCase(String originName) {
+    public static String toBigCamelCase(String originName) {
+        if (StringUtils.isBlank(originName)) {
+            return "";
+        }
+
+        String spaceStr = originName.replaceAll("_", " ").replaceAll("-", " ").replaceAll("\\.", " ").replaceAll(",", " ").replaceAll("\t", " ").trim();
+
+        StringBuffer sb = null;
+        if (!spaceStr.isEmpty()) {
+            String[] split = spaceStr.split(" ");
+            sb = new StringBuffer();
+            for (String s : split) {
+                sb.append(s.substring(0, 1).toUpperCase()).append(s.substring(1).toLowerCase());
+            }
+        }
+
+        return sb == null ? "" : sb.toString();
+    }
+
+    public static String toSmallCamelCase(String originName) {
         if (StringUtils.isBlank(originName)) {
             return "";
         }
