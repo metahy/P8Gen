772557b40d2d2416676884fc56c30df861598139
曹海亮 <1050588656@qq.com>
2020-11-16 23:55:45 +0800

    [OPTIMIZE] change Main.main(), use pattern to generate code.

--- src/com/tools/gen/Main.java
+++ src/com/tools/gen/Main.java
@@ -1,6 +1,11 @@
 package com.tools.gen;
 
+import com.tools.gen.entity.Clazz;
+import com.tools.gen.entity.Interface;
 import com.tools.gen.entity.P8TradeInfo;
+import com.tools.gen.pattern.ClazzGenStrategy;
+import com.tools.gen.pattern.GenContext;
+import com.tools.gen.pattern.InterfaceGenStrategy;
 import com.tools.gen.utils.GenUtils;
 import com.tools.gen.utils.Logger;
 import com.tools.gen.utils.P8TradeInfoReader;
@@ -17,20 +22,33 @@ public class Main {
         P8TradeInfo tradeInfo = P8TradeInfoReader.read("resources/" + fileName);
         Logger.info(tradeInfo);
 
+        GenContext<Clazz> clazzGenContent = new GenContext<>(new ClazzGenStrategy());
+        GenContext<Interface> interfaceGenContent = new GenContext<>(new InterfaceGenStrategy());
+
         // generate java
         // TODO
         System.out.println("----------------------");
-        System.out.println(GenUtils.generate(tradeInfo.getInVo()));
+        System.out.println(clazzGenContent.generate(tradeInfo.getInVo()));
         System.out.println("----------------------");
 
         // TODO
         System.out.println("----------------------");
-        System.out.println(GenUtils.generate(tradeInfo.getOutVo()));
+        System.out.println(clazzGenContent.generate(tradeInfo.getOutVo()));
         System.out.println("----------------------");
 
         // TODO
         System.out.println("----------------------");
-        tradeInfo.getGrpList().forEach(c -> System.out.println(GenUtils.generate(c)));
+        tradeInfo.getGrpList().forEach(c -> System.out.println(clazzGenContent.generate(c)));
         System.out.println("----------------------");
+
+        // TODO
+//        System.out.println("----------------------");
+//        System.out.println(interfaceGenContent.generate(tradeInfo.getService()));
+//        System.out.println("----------------------");
+
+        // TODO
+//        System.out.println("----------------------");
+//        System.out.println(clazzGenContent.generate(tradeInfo.getServiceImpl()));
+//        System.out.println("----------------------");
     }
 }
