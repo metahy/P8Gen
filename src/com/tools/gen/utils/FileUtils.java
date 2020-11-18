b672181598383678fa4a8ee9b92740e7e649baf3
hiramjoyce <chliang0904@sina.com>
2020-11-18 14:47:02 +0800

    [DEVELOP LOG] 增加处理文件的工具类

---
+++ src/com/tools/gen/utils/FileUtils.java
@@ -0,0 +1,44 @@
+package com.tools.gen.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FileUtils {
+    public static void write(byte[] bytes, File file) {
+        if (file == null) {
+            System.out.println(new String(bytes));
+            return;
+        }
+        boolean fileExists = file.exists();
+        if (!fileExists) {
+            File parentFile = file.getParentFile();
+            boolean parentExists = parentFile.exists();
+            if (!parentExists) {
+                parentExists = parentFile.mkdirs();
+            }
+            if (parentExists) {
+                try {
+                    fileExists = file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (fileExists) {
+            try {
+                OutputStream outputStream = new FileOutputStream(file);
+                outputStream.write(bytes);
+                outputStream.flush();
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void delete(File file) {
+        file.deleteOnExit();
+    }
+}
