2210b7678f711ebdaba6a5f527968a4a9b75f959
hiramjoyce <chliang0904@sina.com>
2020-11-18 16:32:53 +0800

    [OPTIMIZE] remove directory before generate

--- src/com/tools/gen/Main.java
+++ src/com/tools/gen/Main.java
@@ -6,13 +6,14 @@ import com.tools.gen.entity.P8TradeInfo;
 import com.tools.gen.pattern.ClazzGenStrategy;
 import com.tools.gen.pattern.GenContext;
 import com.tools.gen.pattern.InterfaceGenStrategy;
-import com.tools.gen.utils.GenUtils;
-import com.tools.gen.utils.Logger;
+import com.tools.gen.utils.FileUtils;
 import com.tools.gen.utils.P8TradeInfoReader;
+
+import java.io.File;
 
 public class Main {
     // generate java code base package
-    public static final String basePackage = "com.ccb.fpp.test";
+    public static final String basePackage = "com.ccb.fpp.cmds";
     // trade source
     public static final String fileName = "P8TradeInfo.txt";
 
@@ -20,10 +21,12 @@ public class Main {
 
         // obtain trade info
         P8TradeInfo tradeInfo = P8TradeInfoReader.read("resources/" + fileName);
-        Logger.info(tradeInfo);
 
         GenContext<Clazz> clazzGenContent = new GenContext<>(new ClazzGenStrategy());
         GenContext<Interface> interfaceGenContent = new GenContext<>(new InterfaceGenStrategy());
+
+        // remove old directory
+        FileUtils.delete(new File("./result/" + basePackage.replaceAll("\\.", "/")));
 
         // generate java
         // TODO
